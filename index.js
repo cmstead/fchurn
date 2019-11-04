e95538d3f9b69551b51e26ec23fc6ff5a92b5105
Chris Stead <cmstead@gmail.com>
2019-11-03 20:36:06 -0800

    Cleaned up logic

--- index.js
+++ index.js
@@ -1,33 +1,76 @@
 const childProcess = require('child_process');
-const gitCommand = `git log --numstat --oneline`;
 
-const logMetrics = childProcess.execSync(gitCommand, { encoding: "utf-8" });
-const dataPattern = /^([0-9]+)\s*([0-9]+)\s*([^\s].+)$/i;
+function getLogMetrics() {
+    const gitCommand = `git log --numstat --oneline`;
 
-const fileMetrics = logMetrics
-    .split('\n')
-    .map(line => 
+    return childProcess.execSync(gitCommand, { encoding: "utf-8" });
+}
+
+function getMetricTokens(logLines) {
+    const dataPattern = /^([0-9]+)\s+([0-9]+)\s+([^\s].+)$/i;
+
+    return logLines
+        .filter(line => {
+            return dataPattern.test(line)
+                && !line.includes('=>');
+        })
+        .map(line =>
             line
-            .trim()
-            .match(dataPattern))
-    .filter(lineValues => lineValues !== null)
-    .map(lineValues => lineValues.slice(1, 4))
-    .reduce((finalMetrics, logValues) => {
-        const fileName = logValues[2];
-        const hasMetrics = typeof finalMetrics[fileName] !== 'undefined';
-        const currentValue = hasMetrics ? finalMetrics[fileName] : 0; 
+                .trim()
+                .match(dataPattern))
+        .filter(lineValues => lineValues !== null);
+}
 
-        finalMetrics[fileName] = currentValue + 1;
+function getMetricValues(metricTokens) {
+    return metricTokens
+        .map(lineValues => lineValues.slice(1, 4));
+}
 
-        return finalMetrics;
-    }, {});
+function buildFileMetricsMap(metricValues) {
+    return metricValues
+        .reduce((finalMetrics, logValues) => {
+            const fileName = logValues[2];
+            const hasMetrics = typeof finalMetrics[fileName] !== 'undefined';
+            const currentValue = hasMetrics ? finalMetrics[fileName] : 0;
 
-const outputValues = Object
-    .keys(fileMetrics)
-    .map(key => [key, fileMetrics[key]]);
+            finalMetrics[fileName] = currentValue + 1;
 
-outputValues.sort((a, b) => b[1] - a[1]);
+            return finalMetrics;
+        }, {});
+}
 
-outputValues.forEach(value => console.log(`\nChurn: ${value[1]} \nFile Name: ${value[0]}`));
+function descendingMetricSort(metric1, metric2) {
+    return metric2.churnCount - metric1.churnCount;
+}
+
+function buildSortedOutputValues(fileMetricsMap) {
+    const extractedOutputValues = Object
+        .keys(fileMetricsMap)
+        .map(key => ({
+            fileName: key,
+            churnCount: fileMetricsMap[key]
+        }));
+
+    extractedOutputValues.sort(descendingMetricSort);
+
+    return extractedOutputValues;
+}
+
+function getSortedLogMetrics() {
+    const logMetrics = getLogMetrics();
+    const logLines = logMetrics.split('\n');
+    const metricTokens = getMetricTokens(logLines);
+
+    console.log(metricTokens);
+
+    const metricValues = getMetricValues(metricTokens);
+    const fileMetricsMap = buildFileMetricsMap(metricValues);
+
+    return buildSortedOutputValues(fileMetricsMap);    
+}
+
+const outputValues = getSortedLogMetrics();
+
+outputValues.forEach(metric => console.log(`\nChurn: ${metric.churnCount} \nFile Name: ${metric.fileName}`));
 
 
